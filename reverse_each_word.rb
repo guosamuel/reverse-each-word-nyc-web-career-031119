cacd1bd7d5abbe0572a8803df29ac4a119266b48
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 16:45:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,9 @@ def reverse_each_word(sentence)
   reverse_words = []
   array_words = sentence.split
   array_words.each do |word|
-    
+    reverse_words.push(word.reverse)
+  end
+  
 end
 
 reverse_each_word("Testing this function")
