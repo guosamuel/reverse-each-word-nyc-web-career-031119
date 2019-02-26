33ea9f177505477febb1670d424bc4fa8ab8a6be
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 16:48:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array_words = sentence.split
   array_words.each collect |word|
-    reverse_words.push(word.reverse)
+    word.reverse
   end
   return array_words.join(" ")
 end
